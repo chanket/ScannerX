36f126c311461a86adee3446d1b198b6caada111
Stardust Sunstrider <chanket@qq.com>
2018-10-22 15:01:06 +0800

    A little change in example program.

--- ScannerX/Program.cs
+++ ScannerX/Program.cs
@@ -17,9 +17,9 @@ namespace ScannerX
             scanner.Timeout = 2000;                                                         //Timeout for TCP connecting.
             scanner.OnResult += Scanner_OnResult;                                           //Fires when successfully connects to an endpoint.
             scanner.Start();                                                                //Start scanning.
-            for (int i = 1; i < 10000; i++)
-            {                                                                               //Add endpoints to the scan list.
-                scanner.Add(new IPEndPoint(IPAddress.Parse("127.0.0.1"), i));               //We can add before or after calling Start(), 
+            for (int i = 1; i < 65536; i++)
+            {                                                                               //Add endpoints of 127.0.0.1 to the scan list.
+                scanner.Add(new IPEndPoint(IPAddress.Loopback, i));                         //We can add before or after calling Start(), 
             }                                                                               //as long as Stop() has not been called.
 
             while (scanner.Count != 0)                                                      //Track status.
