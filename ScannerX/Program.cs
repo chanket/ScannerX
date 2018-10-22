1a28d39b29343c96a0d7636724b53eece85d9ee6
Stardust Sunstrider <chanket@qq.com>
2018-10-22 14:09:40 +0800

    First version

--- ScannerX/Program.cs
+++ ScannerX/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ScannerX
@@ -10,6 +13,36 @@ namespace ScannerX
     {
         static void Main(string[] args)
         {
+            Scanner scanner = new Scanner(2000);                                            //A new scanner with maximum 2000 sockets in parallel.
+            scanner.Timeout = 2000;                                                         //Timeout for TCP connecting.
+            scanner.OnResult += Scanner_OnResult;                                           //Fires when successfully connects to an endpoint.
+            scanner.Start();                                                                //Start scanning.
+            for (int i = 1; i < 10000; i++)
+            {                                                                               //Add endpoints to the scan list.
+                scanner.Add(new IPEndPoint(IPAddress.Parse("127.0.0.1"), i));               //We can add before or after calling Start(), 
+            }                                                                               //as long as Stop() has not been called.
+
+            while (scanner.Count != 0)                                                      //Track status.
+            {
+                Thread.Sleep(50);
+                lock (scanner)
+                {
+                    Console.Write("Remaining: " + scanner.Count.ToString() + "      \r");   
+                }
+            }
+            scanner.Stop();                                                                 //Stop scanning.
+
+            Console.WriteLine("Mission accomplished.");
+            Console.ReadKey();
+        }
+
+        private static void Scanner_OnResult(Scanner from, IPEndPoint ep)
+        {
+            lock (from)
+            {
+                Console.Write("                                                       \r");
+                Console.WriteLine(ep.ToString());
+            }
         }
     }
 }
---
+++ ScannerX/Scanner.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScannerX
+{
+    class Scanner
+    {
+        #region Delegates And Events
+
+        public delegate void ScannerResultDelegate(Scanner from, IPEndPoint ep);
+
+        public event ScannerResultDelegate OnResult;
+
+        #endregion
+
+        /// <summary>
+        /// Cancels when calling <code>Stop</code> method.
+        /// </summary>
+        protected CancellationTokenSource TokenSource { get; } = new CancellationTokenSource();
+
+        /// <summary>
+        /// The list storing <code>IPEndPoint</code>s to be scanned.
+        /// </summary>
+        protected LinkedList<IPEndPoint> ScanList { get; } = new LinkedList<IPEndPoint>();
+
+        /// <summary>
+        /// The semaphore to control parallelism.
+        /// </summary>
+        protected SemaphoreSlim SemaphoreParallelism { get; }
+
+        /// <summary>
+        /// The semaphore indicating the amount of elements in <code>ScanList</code>.
+        /// </summary>
+        protected SemaphoreSlim SemaphoreScanList { get; }
+        
+        /// <summary>
+        /// Maximum degree of parallelism.
+        /// </summary>
+        public int DegreeOfParallelism { get; }
+
+        /// <summary>
+        /// Whether the instance has called <code>Start</code>.
+        /// </summary>
+        public bool IsStarted { get; protected set; } = false;
+
+        /// <summary>
+        /// Whether the instance has called <code>Stop</code>.
+        /// </summary>
+        public bool IsStopped { get; protected set; } = false;
+
+        /// <summary>
+        /// Timeout in miliseconds when connecting to an <code>IPEndPoint</code>.
+        /// </summary>
+        public int Timeout { get; set; } = 3000;
+
+        /// <summary>
+        /// Remaining amount of <code>IPEndPoint</code>s to be scanned.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return SemaphoreScanList.CurrentCount       //To be scanned
+                    + WorkingWorkers;                       //Is scanning
+            }
+        }
+
+        /// <summary>
+        /// Workers that is scanning.
+        /// </summary>
+        public int WorkingWorkers
+        {
+            get
+            {
+                return DegreeOfParallelism - SemaphoreParallelism.CurrentCount;
+            }
+        }
+
+        /// <summary>
+        /// Creating an instance.
+        /// </summary>
+        /// <param name="degreeOfParallelism">The maximum degree of parallelism.</param>
+        public Scanner(int degreeOfParallelism)
+        {
+            DegreeOfParallelism = degreeOfParallelism;
+            SemaphoreParallelism = new SemaphoreSlim(DegreeOfParallelism);
+            SemaphoreScanList = new SemaphoreSlim(0);
+        }
+
+        /// <summary>
+        /// Add <paramref name="ep"/> to the scan list. 
+        /// </summary>
+        public void Add(IPEndPoint ep)
+        {
+            lock (ScanList)
+            {
+                ScanList.AddLast(ep);
+            }
+            SemaphoreScanList.Release();
+        }
+
+        /// <summary>
+        /// Start scanning.
+        /// </summary>
+        public void Start()
+        {
+            if (!IsStarted)
+            {
+                IsStarted = true;
+                Loop();
+            }
+        }
+
+        /// <summary>
+        /// Stop scanning.
+        /// </summary>
+        public void Stop()
+        {
+            if (!IsStopped)
+            {
+                IsStopped = true;
+                TokenSource.Cancel();
+            }
+        }
+
+        /// <summary>
+        /// Scanning loop.
+        /// </summary>
+        protected async void Loop()
+        {
+            while (!IsStopped)
+            {
+                try
+                {
+                    await SemaphoreScanList.WaitAsync(TokenSource.Token).ConfigureAwait(false);
+                    IPEndPoint ep;
+                    lock (ScanList)
+                    {
+                        ep = ScanList.First.Value;
+                        ScanList.RemoveFirst();
+                    }
+
+                    await SemaphoreParallelism.WaitAsync(TokenSource.Token).ConfigureAwait(false);
+                    Scan(ep);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Try connecting to <paramref name="ep"/> and invokes <code>OnResult</code> event if success.
+        /// </summary>
+        protected async void Scan(IPEndPoint ep)
+        {
+            try
+            {
+                using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    client.BeginConnect(ep, null, null);
+                    await Task.Delay(Timeout, TokenSource.Token).ConfigureAwait(false);
+                    if (client.Connected)
+                    {
+                        OnResult?.BeginInvoke(this, ep, null, null);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                SemaphoreParallelism.Release();
+            }
+        }
+    }
+}
