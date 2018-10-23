e351b124f1a1909277f03dd0b83abac4ab69c094
Stardust Sunstrider <chanket@qq.com>
2018-10-23 14:06:10 +0800

    edit comments

--- ScannerX/Program.cs
+++ ScannerX/Program.cs
@@ -13,21 +13,22 @@ namespace ScannerX
     {
         static void Main(string[] args)
         {
-            Scanner scanner = new Scanner(2000);                                            //A new scanner with maximum 2000 sockets in parallel.
-            scanner.Timeout = 2000;                                                         //Timeout for TCP connecting.
+            Scanner scanner = new Scanner(2000);                                            //A new scanner with maximum 2000 sockets.
+            scanner.Timeout = 2000;                                                         //Timeout 2000ms for TCP connecting.
             scanner.OnResult += Scanner_OnResult;                                           //Fires when successfully connects to an endpoint.
-            scanner.Start();                                                                //Start scanning.
-            for (int i = 1; i < 65536; i++)
+
+            for (int i = 1; i < 10000; i++)
             {                                                                               //Add endpoints of 127.0.0.1 to the scan list.
                 scanner.Add(new IPEndPoint(IPAddress.Loopback, i));                         //We can add before or after calling Start(), 
             }                                                                               //as long as Stop() has not been called.
 
-            while (scanner.Count != 0)                                                      //Track status.
+            scanner.Start();                                                                //Start scanning.
+            while (scanner.Count != 0)                                                      
             {
                 Thread.Sleep(50);
                 lock (scanner)
                 {
-                    Console.Write("Remaining: " + scanner.Count.ToString() + "      \r");   
+                    Console.Write("Remaining: " + scanner.Count.ToString() + "      \r");   //Track status.
                 }
             }
             scanner.Stop();                                                                 //Stop scanning.
